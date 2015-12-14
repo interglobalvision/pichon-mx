0d49a19e3667459066510858f6eb89cbf0a06809
Patrick Best <patrickbest@patrickbest.com>
2015-12-14 17:22:58 -0600

    add ingredients taxonomy

---
+++ lib/taxonomies.php
@@ -0,0 +1,40 @@
+<?php
+
+// Register Custom Taxonomy
+function ingredient_taxonomy() {
+
+	$labels = array(
+		'name'                       => 'Ingredients',
+		'singular_name'              => 'Ingredient',
+		'menu_name'                  => 'Ingredients',
+		'all_items'                  => 'All Items',
+		'parent_item'                => 'Parent Item',
+		'parent_item_colon'          => 'Parent Item:',
+		'new_item_name'              => 'New Item Name',
+		'add_new_item'               => 'Add New Item',
+		'edit_item'                  => 'Edit Item',
+		'update_item'                => 'Update Item',
+		'view_item'                  => 'View Item',
+		'separate_items_with_commas' => 'Separate items with commas',
+		'add_or_remove_items'        => 'Add or remove items',
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular Items',
+		'search_items'               => 'Search Items',
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No items',
+		'items_list'                 => 'Items list',
+		'items_list_navigation'      => 'Items list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'ingredient', array( 'recipe' ), $args );
+
+}
+add_action( 'init', 'ingredient_taxonomy', 0 );
