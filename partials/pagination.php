ac91eac0a57b213811148a54f0ba50c4bcece34f
Patrick Best <patrickbest@patrickbest.com>
2016-03-03 14:07:03 -0600

    style pagination
    missing pagination color going escaping the container

--- partials/pagination.php
+++ partials/pagination.php
@@ -1,7 +1,9 @@
 <?php
 if (get_next_posts_link() || get_previous_posts_link()) {
-  $previous = get_previous_posts_link('Newer');
-  $next = get_next_posts_link('Older');
+
+  $previous = get_previous_posts_link(__('[:es]<span class="arrow">&#8606;</span> Anterior[:en]<span class="arrow">&#8606;</span> Previous'));
+  $next = get_next_posts_link(__('[:es]Siguente <span class="arrow">&#8608;</span>[:en]Next <span class="arrow">&#8608;</span>'));
+
 ?>
   <!-- post pagination -->
   <nav id="pagination" class="border-bottom">
@@ -10,7 +12,7 @@ if (get_next_posts_link() || get_previous_posts_link()) {
         <?php
           if ($previous) {
         ?>
-        <div class="col s-col7 border-right force-col background-yellow">
+        <div class="col s-col7 pagination-block fonts-sans border-right force-col background-yellow flex-center">
           <?php echo $previous; ?>
         </div>
         <div class="col s-col10 force-col"></div>
@@ -21,7 +23,7 @@ if (get_next_posts_link() || get_previous_posts_link()) {
         <?php
           }
         ?>
-        <div class="col s-col7 border-left force-col background-blue">
+        <div class="col s-col7 pagination-block font-sans border-left force-col background-blue flex-center">
           <?php
             if ($next) {
               echo $next;
