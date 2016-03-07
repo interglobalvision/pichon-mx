3afab08de566017966821719c1aa51d1cc419136
Patrick Best <patrickbest@patrickbest.com>
2016-03-07 13:52:52 -0600

    add layout logic for equal heights

--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
 /* jshint browser: true, devel: true, indent: 2, curly: true, eqeqeq: true, futurehostile: true, latedef: true, undef: true, unused: true */
 /* global $, jQuery, document, Modernizr */
 
+var Layout = {
+  init: function() {
+    var _this = this;
+
+    _this.equalizeRowHeights();
+
+  },
+
+  bind: function() {
+    var _this = this;
+
+    $(window).resize(function() {
+    _this.equalizeRowHeights();
+    });
+
+  },
+
+  equalizeRowHeights: function() {
+    var $targets = $('.row');
+
+    $targets.each(function(index, item) {
+      var $children = $(item).children();
+      var maxHeight = 0;
+
+      $children.each(function() {
+        var height = $(this).height();
+
+        if (height > maxHeight){
+          maxHeight = height;
+        }
+      });
+
+      $children.css('height', maxHeight);
+
+    });
+  },
+};
+
 jQuery(document).ready(function () {
   'use strict';
-  l('Hola Globie');
+
+  Layout.init();
+
 });
