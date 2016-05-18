dffeb0cde6138510e986aa41867bd32240cf476c
Michael Ray-Von <mikey@interglobal.vision>
2016-05-18 23:28:05 +0300

    add loading overlay to markup

--- header.php
+++ header.php
@@ -51,6 +51,8 @@
 <body <?php body_class(); ?>>
 <!--[if lt IE 9]><p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
 
+  <div class="loading-overlay"></div>
+
   <section id="main-container">
 
   <!-- start content -->
