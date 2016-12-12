cf108c92c478530b67104c9846370bdba142baf0
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-12-12 14:33:37 -0600

    fix add_action priority for php libs

--- functions.php
+++ functions.php
@@ -55,14 +55,14 @@ get_template_part( 'lib/taxonomies' );
 get_template_part( 'lib/meta-boxes' );
 get_template_part( 'lib/theme-options' );
 
-add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
+add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 11);
 function cmb_initialize_cmb_meta_boxes() {
   // Add CMB2 plugin
   if( ! class_exists( 'cmb2_bootstrap_202' ) )
     require_once 'lib/CMB2/init.php';
 }
 
-add_action( 'init', 'init_moment_php', 9999 );
+add_action( 'init', 'init_moment_php', 11);
 function init_moment_php() {
   if ( ! class_exists( 'Moment' ) )
     require_once 'lib/moment-php/src/Moment.php';
